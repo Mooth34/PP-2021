cf45b52be35f3c6263e8c48fd9a6106c38df3e20
Mooth <astashoff.misha@gmail.com>
2021-12-01 22:03:33 +0500

    non-parallel code of GasStation done

--- lab3/src/main/java/GasStation.java
+++ lab3/src/main/java/GasStation.java
@@ -7,55 +7,58 @@ public class GasStation {
     public static final int PRICE = 50;
     private int gas_count = INITIAL_GAS_VOLUME;
 
-    public void sendGasToPump(Pump pump, int gas_count) throws OutOfGasException{
-        if (this.gas_count >= gas_count)
-            this.gas_count-=gas_count;
-        else {
-            System.out.println("Out of gas!");
-        }
-
+    public void setGas_count(int gas_count) {
+        this.gas_count = gas_count;
     }
 
     static class Cashier{
-        private int money;
+       private int money = 0;
 
-       public Cashier(int money) {
-           super();
-           this.money = money;
+       public Cashier() {}
+
+        public int getMoney() {
+            return money;
+        }
+
+        private Pump sendGasToPump(GasStation gas_station, Buyer buyer, int gas_count) throws Exception {
+            if (gas_station.gas_count >= gas_count) {
+                gas_station.gas_count -= gas_count;
+                //Выбор свободного насоса Pump pump = find();
+                pump.prepareGas(buyer, gas_count);
+                buyer.attachToPump(pump);
+                return pump;
+            }
+            else
+                throw new Exception("Out of gas!");
        }
-       public int getMoney() {
-           return money;
-       }
-       public  void setMoney(int money) {
-           this.money = money;
-       }
+
+        public void serveCustomer(GasStation gas_station, Buyer buyer, int gas_count) throws Exception {
+           money+=buyer.payForGas(gas_count, this);
+           sendGasToPump(gas_station, buyer, gas_count);
+        }
     }
 
-    static class Buyer implements Runnable{
+    static class Buyer{
         int gas_count = 0;
         int money = INITIAL_WEALTH;
         private Pump pump;
 
-        public Buyer(Pump pump) {
-            super();
-            this.pump = pump;
-        }
+        public Buyer() {}
 
-        @Override
-        public void run() {
-
-            while(!Thread.currentThread().isInterrupted() && canProceed()) {
-                pump.dispenseGas(this.pump, this, gas_amount);
+        public int payForGas(int gas_count, Cashier cashier) throws Exception {
+            if (PRICE * gas_count < money) {
+                money -= PRICE * gas_count;
+                return PRICE * gas_count;
             }
-
+            else throw new Exception("Not enough money!");
         }
 
-        public void addGas(int i) {
+        public void obtainGas(int i) {
             gas_count += i;
         }
 
-        private boolean canProceed() {
-            return !acc.isEmpty()  && seller.getNumOfGoods() > 0;
+        public void attachToPump(Pump pump){
+            this.pump = pump;
         }
 
     }
@@ -63,25 +66,36 @@ public class GasStation {
 
     static class Pump implements Runnable{
         private int gas_volume;
+        private Buyer current_buyer;
 
-        public void addGas(Pump pump, Buyer buyer, int gas_amount){
-            buyer.addGas(gas_amount);
-            pump.
+        public void prepareGas(Buyer buyer, int gas_amount){
+            this.gas_volume+=gas_amount;
+            this.current_buyer = buyer;
         }
 
-        public void dispenseGas(Pump pump, Buyer buyer, int gas_amount){
-            buyer.addGas(gas_amount);
-            this.gas_volume-=gas_amount
+        public void dispenseGas() throws Exception {
+            if (gas_volume > 0) {
+                this.current_buyer.obtainGas(gas_volume);
+                this.gas_volume -= gas_volume;
+                this.current_buyer.obtainGas(gas_volume);
+            }
+            else throw new Exception("No gas in pump!");
         }
 
         @Override
         public void run() {
-
+            while(!Thread.currentThread().isInterrupted()) {
+                try {
+                    this.dispenseGas();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
-
-
-    List<Pump> pumps = new LinkedList<>();
-    List<Buyer> buyers = new LinkedList<>();
+    public static void main(String[] args) throws InterruptedException {
+        List<Pump> pumps = new LinkedList<>();
+        List<Buyer> buyers = new LinkedList<>();
+    }
 }
